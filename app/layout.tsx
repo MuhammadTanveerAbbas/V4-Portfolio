ea42fecd43ee14f46723ccbf4e3974ab0f50ff31
MuhammadTanveerAbbas <muhammadtanveerabbas.dev@gmail.com>
2026-03-26 22:48:46 +0500

    Patch Updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,10 +101,12 @@ export const metadata: Metadata = {
 
   icons: {
     icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/icon-light-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/icon.svg", type: "image/svg+xml" },
     ],
-    shortcut: "/icon-light-32x32.png",
+    shortcut: "/favicon.ico",
     apple: "/apple-icon.png",
   },
 };
